9f3d3eead88ea68d77d3cf89313b411ae8ed820e
onix.develop@gmail.com <H53Oy2Z1ChNbYS8asv1W6bgYUqEnHze0BtEP>
2020-03-27 14:00:05 +0200

    Chapter 4: Database

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://postgres:12345@localhost/app'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
