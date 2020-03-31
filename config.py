a0e2e0676d13ef79ed3a015409292a19bd071663
onix.develop@gmail.com <H53Oy2Z1ChNbYS8asv1W6bgYUqEnHze0BtEP>
2020-03-31 10:42:09 +0300

    Chapter 7: Error Handling

--- config.py
+++ config.py
@@ -7,3 +7,10 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://postgres:12345@localhost/app'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
