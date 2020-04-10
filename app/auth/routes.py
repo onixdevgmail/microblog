cfcf8c6e13e0f1830882f1573e632999d82b1bec
onix.develop@gmail.com <H53Oy2Z1ChNbYS8asv1W6bgYUqEnHze0BtEP>
2020-04-10 17:14:33 +0300

    Google login

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,14 +1,15 @@
 from flask import render_template, redirect, url_for, flash, request
+from flask_babel import _
+from flask_login import login_user, logout_user, current_user
 from sqlalchemy.orm.exc import NoResultFound
 from werkzeug.urls import url_parse
-from flask_login import login_user, logout_user, current_user
-from flask_babel import _
+
 from app import db
 from app.auth import bp
+from app.auth.email import send_password_reset_email
 from app.auth.forms import LoginForm, RegistrationForm, \
     ResetPasswordRequestForm, ResetPasswordForm
 from app.models import User
-from app.auth.email import send_password_reset_email
 
 
 @bp.route('/login', methods=['GET', 'POST'])
@@ -116,13 +117,9 @@ import config
 
 
 def get_google_provider_cfg():
-    # return requests.get(GOOGLE_DISCOVERY_URL).json()
-
-    # return requests.get(config['GOOGLE_DISCOVERY_URL']).json()
     return requests.get(config.Config.GOOGLE_DISCOVERY_URL).json()
 
 
-# client = WebApplicationClient(GOOGLE_CLIENT_ID)
 client = WebApplicationClient(config.Config.GOOGLE_CLIENT_ID)
 
 
@@ -163,8 +160,6 @@ def callback():
         token_url,
         headers=headers,
         data=body,
-        # auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
-        # auth=(config['GOOGLE_CLIENT_ID'], config['GOOGLE_CLIENT_SECRET']),
         auth=(config.Config.GOOGLE_CLIENT_ID, config.Config.GOOGLE_CLIENT_SECRET),
     )
 
