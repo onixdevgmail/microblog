fb6b6b5dba5b16998f0bcf339ec15aa982f11f99
onix.develop@gmail.com <H53Oy2Z1ChNbYS8asv1W6bgYUqEnHze0BtEP>
2020-04-10 17:10:06 +0300

    Google login

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, redirect, url_for, flash, request
+from sqlalchemy.orm.exc import NoResultFound
 from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_babel import _
@@ -106,3 +107,104 @@ def reset_password(token):
         flash(_('Your password has been reset.'))
         return redirect(url_for('auth.login'))
     return render_template('auth/reset_password.html', form=form)
+
+
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+import json
+import config
+
+
+def get_google_provider_cfg():
+    # return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+    # return requests.get(config['GOOGLE_DISCOVERY_URL']).json()
+    return requests.get(config.Config.GOOGLE_DISCOVERY_URL).json()
+
+
+# client = WebApplicationClient(GOOGLE_CLIENT_ID)
+client = WebApplicationClient(config.Config.GOOGLE_CLIENT_ID)
+
+
+@bp.route("/google_login")
+def google_login():
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+
+
+@bp.route("/google_login/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    # Prepare and send a request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        # auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+        # auth=(config['GOOGLE_CLIENT_ID'], config['GOOGLE_CLIENT_SECRET']),
+        auth=(config.Config.GOOGLE_CLIENT_ID, config.Config.GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # Now that you have tokens (yay) let's find and hit the URL
+    # from Google that gives you the user's profile information,
+    # including their Google profile image and email
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    if userinfo_response.json().get("email_verified"):
+        users_email = userinfo_response.json()["email"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    # Create a user in your db with the information provided
+    # by Google
+    # user = User(
+    #         id_=unique_id, name=users_name, email=users_email )
+
+    # Doesn't exist? Add it to the database.
+    # if not User.query.filter_by(id=unique_id).first():
+    #     User.create(unique_id, users_name, users_email)
+
+    query = User.query.filter_by(username=users_name)
+
+    try:
+        user = query.one()
+    except NoResultFound:
+        user = User(username=users_name, email=users_email)
+        db.session.add(user)
+        db.session.commit()
+
+    # Begin user session by logging the user in
+    login_user(user)
+
+    # Send user back to homepage
+
+    return redirect(url_for('main.index'))
